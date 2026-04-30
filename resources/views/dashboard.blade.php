648a199133426818066cb2e1d714708562a31c4c
asywrq <anwarsyafiq2605@gmail.com>
2026-04-30 21:40:09 +0800

    Initial commit: AdminKit and Jetstream

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('master.layout')
+
+@section('content')
+    <div class="container-fluid p-0">
+        <h1 class="h3 mb-3">OSEM Dashboard</h1>
+        
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        Welcome to the OSEM System! You are logged in.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

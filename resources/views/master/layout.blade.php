e548e3662aaa92e4e806cd25f689c67cc31c50c1
asywrq <anwarsyafiq2605@gmail.com>
2026-06-11 09:18:05 +0800

    remove My Appeal in user menu

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -153,12 +153,6 @@
                             <span class="align-middle">My Compounds</span>
                         </a>
                     </li>
-                    <li class="sidebar-item {{ request()->routeIs('user.appeal') ? 'active' : '' }}">
-                        <a class="sidebar-link" href="{{ route('user.appeal') }}">
-                            <i class="align-middle" data-feather="message-square"></i>
-                            <span class="align-middle">Appeal</span>
-                        </a>
-                    </li>
                 @endif
 
             </ul>
